dc13fbe8d2df1eeeae017dd3d6ff514d465c3bc0
KAUNGPYAEPAING <88584127+KAUNGPYAEPAING@users.noreply.github.com>
2021-09-13 16:37:42 +0630

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function housekeeper(name, age, language) {
+  this.name : name;
+  this.age  : age;
+  this.language : language;
+  this.movesuitcase : function movesuitcase (){
+    alert ("May I take your suit case");
+    Pickupsuitcase();
+    move();
+  }
+}
