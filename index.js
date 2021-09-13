dc28dcab83e64144b9b30938aece5357c76059cc
KAUNGPYAEPAING <88584127+KAUNGPYAEPAING@users.noreply.github.com>
2021-09-13 16:40:37 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function housekeeper(name, age, language) {
-  this.name : name;
-  this.age  : age;
-  this.language : language;
-  this.movesuitcase : function movesuitcase (){
+  this.name = name;
+  this.age  = age;
+  this.language = language;
+  this.movesuitcase = function movesuitcase (){
     alert ("May I take your suit case");
     Pickupsuitcase();
     move();
